2136e7c83a05c7fe32eb7b36e607a3fcf56b168b
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-14 12:53:57 -0300

    feat: Criado componente meu modal para utilizar ao abrir a página.

---
+++ questao-9/src/app/componentes/meu-modal/meu-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-modal.component.html',
+  styleUrl: './meu-modal.component.css'
+})
+export class MeuModalComponent {
+
+}
