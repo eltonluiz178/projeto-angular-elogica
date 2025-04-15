cd64e8df68aeabe6c3dfed0156f7c2c6af2b2319
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-15 16:09:19 -0300

    feat: Adiciona PensamentoProfile para mapeamento das classes Pensamento, PensamentoDto com AutoMapper.

---
+++ API-Memoteca/API-Memoteca/Profiles/PensamentoProfile.cs
@@ -0,0 +1,13 @@
+﻿using API_Memoteca.Domain;
+using API_Memoteca.Dto;
+using AutoMapper;
+
+namespace API_Memoteca.Profiles;
+    
+public class PensamentoProfile : Profile
+{
+    public PensamentoProfile()
+    {
+        CreateMap<Pensamento, PensamentoDto>().ReverseMap();
+    }
+}
