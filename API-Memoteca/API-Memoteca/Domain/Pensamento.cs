3e2211e5ff1d5498685e292af194756dcad73679
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-15 16:04:24 -0300

    feat: Adiciona classes Pensamento e RetornoPensamentoPaginado.

---
+++ API-Memoteca/API-Memoteca/Domain/Pensamento.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Memoteca.Domain;
+
+public class Pensamento
+{
+    public int Id { get; set; }
+    public string Pensamento { get; set; }
+    public string Autor { get; set; }
+    public int Modelo { get; set; }
+}
---
+++ API-Memoteca/API-Memoteca/Domain/RetornoPensamentoPaginado.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Memoteca.Domain;
+
+public class RetornoPensamentoPaginado<Pensamento>
+{
+    public int TotalRegistros { get; set; }
+    public int Pagina { get; set; }
+    public int QtdPagina { get; set; }
+    public List<Pensamento> Pensamentos { get; set; }
+}
