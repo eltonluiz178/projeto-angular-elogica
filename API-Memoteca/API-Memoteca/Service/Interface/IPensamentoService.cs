8a9af3e9277e3f561ff4a638fa8a5f0bf9666d0a
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-15 16:10:23 -0300

    feat: Adiciona serviço de Pensamento com operações CRUD assíncronas.

---
+++ API-Memoteca/API-Memoteca/Service/Interface/IPensamentoService.cs
@@ -0,0 +1,13 @@
+﻿using API_Memoteca.Domain;
+
+namespace API_Memoteca.Service.Interface;
+
+public interface IPensamentoService
+{
+    Task<RetornoPensamentoPaginado<Pensamento>> BuscarPensamentoPaginadoASync(int pagina, int quantidade);
+    Task<List<Pensamento>> BuscarTodosPensamentosASync();
+    Task<Pensamento> BuscarPensamentoPorIdASync(int id);
+    Task<bool> AdicionarPensamentoASync(Pensamento pensamento);
+    Task<bool> EditarPensamentoASync(Pensamento pensamento);
+    Task<bool> ExcluirPensamentoASync(int id);
+}
---
+++ API-Memoteca/API-Memoteca/Service/Service/PensamentoService.cs
@@ -0,0 +1,123 @@
+﻿using API_Memoteca.Domain;
+using API_Memoteca.Infra.Interfaces;
+using API_Memoteca.Service.Interface;
+
+namespace API_Memoteca.Service.Service;
+
+public class PensamentoService : IPensamentoService
+{
+    private readonly IPensamentoRepository _repository;
+
+    public PensamentoService(IPensamentoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<bool> AdicionarPensamentoASync(Pensamento pensamento)
+    {
+        try
+        {
+            var resposta = await _repository.AdicionarPensamento(pensamento);
+            if (resposta == null || resposta == false)
+            {
+                throw new Exception("Não foi possível localizar o pensamento.");
+            }
+            else
+            {
+                return resposta;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<RetornoPensamentoPaginado<Pensamento>> BuscarPensamentoPaginadoASync(int pagina, int quantidade)
+    {
+        try
+        {
+            var pensamentos = await _repository.BuscarPensamentoPaginado(pagina, quantidade);
+            if(pensamentos == null)
+            {
+                throw new Exception("Não foi possível localizar nenhum pensamento.");
+            }
+            else
+            {
+                return pensamentos;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<Pensamento> BuscarPensamentoPorIdASync(int id)
+    {
+        try
+        {
+            var pensamento = await _repository.BuscarPensamentoPorId(id);
+            if(pensamento == null)
+            {
+                throw new Exception("Pensamento não foi encontrado");
+            }
+            else
+            {
+                return pensamento;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<List<Pensamento>> BuscarTodosPensamentosASync()
+    {
+        try
+        {
+            var pensamentos = await _repository.BuscarTodosPensamentos();
+            if(pensamentos == null)
+            {
+                throw new Exception("Não foi possível localizar nenhum pensamento.");
+            }
+            else
+            {
+                return pensamentos;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<bool> EditarPensamentoASync(Pensamento pensamento)
+    {
+        try
+        {
+            var resposta = await _repository.EditarPensamento(pensamento);
+            if(resposta == null || resposta == false)
+            {
+                throw new Exception("Não foi possível localizar o pensamento.");
+            }
+            else
+            {
+                return resposta;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<bool> ExcluirPensamentoASync(int id)
+    {
+        try
+        {
+            var resposta = await _repository.ExcluirPensamento(id);
+            if (resposta == null || resposta == false)
+            {
+                throw new Exception("Não foi possível localizar o pensamento.");
+            }
+            else
+            {
+                return resposta;
+            }
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+}
