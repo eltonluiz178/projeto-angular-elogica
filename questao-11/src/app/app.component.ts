587c5d5fb55f6e26ecf68c0391184748ab141068
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-16 13:01:50 -0300

    feat: Adicionando componentes do Cabecalho e Formulario ao app imports

--- questao-11/src/app/app.component.ts
+++ questao-11/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./shared/componentes/cabecalho/cabecalho.component";
+import { FormularioComponent } from "./pages/formulario/formulario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CabecalhoComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
