7be9e0350617459e4a5afbce2ebc0760a9388e84
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-16 14:43:17 -0300

    feat: Definir rotas para os componentes Mural e Formulário

--- questao-11/src/app/app.routes.ts
+++ questao-11/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MuralComponent } from './pages/mural/mural.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/mural',
+    pathMatch: 'full'
+  },
+  {
+  path: 'mural',
+  component: MuralComponent
+  },
+  {
+    path: 'formulario',
+    component: FormularioComponent
+  }
+];
