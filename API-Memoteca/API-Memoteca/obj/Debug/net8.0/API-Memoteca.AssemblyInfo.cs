b20dccc4f3ebe0de59c34910d8520c757d7dbed8
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-17 16:10:42 -0300

    Atualização de versão e reorganização de documentos
    - Atualização da versão informacional do assembly para `1.0.0+dfffdd2684179c609c9f6d1c62919570ae5b977e`.
    - Modificações em arquivos binários relacionados ao estado do banco de dados, configurações do projeto e compilação.
    - Reorganização de documentos nos arquivos `DocumentLayout.json` e `DocumentLayout.backup.json`, incluindo novos documentos, ajustes de índices e metadados.
    - Atualização do mapeamento de sourcelink para apontar para o novo commit `dfffdd2684179c609c9f6d1c62919570ae5b977e`.
    - Alteração no cache de inputs do assembly, refletindo mudanças no projeto.

--- API-Memoteca/API-Memoteca/obj/Debug/net8.0/API-Memoteca.AssemblyInfo.cs
+++ API-Memoteca/API-Memoteca/obj/Debug/net8.0/API-Memoteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Memoteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12278317604a56684af6f007fafc8712178994ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfffdd2684179c609c9f6d1c62919570ae5b977e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Memoteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Memoteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
